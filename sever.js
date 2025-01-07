beca1e4fa8c0e91f661f6988d352154394e92172
itsprestonjacobs <143894974+itsprestonjacobs@users.noreply.github.com>
2025-01-07 12:03:51 -0600

    Create sever.js

---
+++ sever.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('offer', (offer) => {
+    socket.broadcast.emit('offer', offer);
+  });
+
+  socket.on('answer', (answer) => {
+    socket.broadcast.emit('answer', answer);
+  });
+
+  socket.on('candidate', (candidate) => {
+    socket.broadcast.emit('candidate', candidate);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
